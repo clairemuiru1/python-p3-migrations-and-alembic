eb2db523350a1bd5c67bb01436baf9f3b5404145
clairemuiru1 <muiruclaire@gmail.com>
2023-12-08 00:39:30 +0300

    "Done with assignment"

---
+++ app/migrations/versions/7005fc0db594_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 7005fc0db594
+Revises: 
+Create Date: 2023-12-06 15:50:40.782253
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7005fc0db594'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
